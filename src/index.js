38bc26a6c97da46b3860d7a6fb4e17c18c02f6fa
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-18 02:41:35 +0300

    get toy data from db

--- src/index.js
+++ src/index.js
@@ -12,4 +12,34 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => {
+    toys.forEach(toy => {
+      let collection = document.querySelector('#toy-collection')
+      let card = document.createElement('div')
+      let h2 = document.createElement('h2')
+      let img = document.createElement('img')
+      let p = document.createElement('p')
+      let button = document.createElement('button')
+
+      card.className = 'card'
+      img.className = 'toy-avatar'
+      button.className = "like-btn"
+      button.id = toy['id']
+
+      h2.innerText = toy['name']
+      img.src = toy['image']
+      p.innerText = toy['likes']
+      button.innerText = 'like'
+
+      card.appendChild(h2)
+      card.appendChild(img)
+      card.appendChild(p)
+      card.appendChild(button)
+      collection.appendChild(card)
+    })
+  })
+
 });
